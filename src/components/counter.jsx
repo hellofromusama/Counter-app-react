b2a538edd1a3db055c1dd74e699e5f2f7bdcacdb
hellofromusama <hellofromusama@gmail.com>
2022-02-08 14:57:48 +0500

    decrement button disabled when less than zero

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,30 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
 
-
   render () {
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm m-2'>
+      <div className='row'>
+        <div className='col-1'>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className='col'>
+          <button onClick={() => this.props.onIncrement(this.props.counter)}
+            className='btn btn-secondary btn-sm'
+          >
           +
         </button>
-        <button onClick={() => this.props.onDecrement(this.props.counter)} className='btn btn-secondary btn-sm m-2'>
+          <button onClick={() => this.props.onDecrement(this.props.counter)}
+            className='btn btn-secondary btn-sm m-2'
+          disabled={this.props.counter.value === 0 ? 'disabled' : ''}
+          >
           -
         </button>
-        <button onClick={() => this.props.onDelete(this.props.counter.id)} className='btn btn-danger btn-sm'>
+          <button onClick={() => this.props.onDelete(this.props.counter.id)}
+            className='btn btn-danger btn-sm'>
           Delete
         </button>
+        </div>
+
       </div>
     )
   }
@@ -30,7 +40,6 @@ class Counter extends Component {
     const { value: count } = this.props.counter
     return count === 0 ? <h1 className='badge'>Zero</h1> : count
   }
-
 
 }
 
